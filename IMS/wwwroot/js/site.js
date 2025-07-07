d2c80f063ef1887326d23ddb1dae784ecbdd40ef
STM02 <STM02@DESKTOP-G861K25>
2025-07-07 09:53:04 +0330

    Add new view for subsystem access page

--- IMS/wwwroot/js/site.js
+++ IMS/wwwroot/js/site.js
@@ -1,4 +1,15 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const modeToggle = document.getElementById('mode-toggle');
 
-// Write your JavaScript code.
+// بررسی حالت ذخیره شده و اعمال آن
+if (localStorage.getItem('darkMode') === 'true') {
+    document.body.classList.add('dark-mode');
+}
+
+// تغییر حالت تاریک/روشن و ذخیره در localStorage
+modeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    const isDarkMode = document.body.classList.contains('dark-mode');
+    localStorage.setItem('darkMode', isDarkMode);
+});
+
+
