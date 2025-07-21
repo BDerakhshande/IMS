9a0817c0d816ea667a940134acf7ead6a39dd27c
STM02 <STM02@DESKTOP-G861K25>
2025-07-21 12:10:43 +0330

    reaport for project

---
+++ IMS.Application/WarehouseManagement/Services/IWarehouseTransactionDetailService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IMS.Application.WarehouseManagement.DTOs;
+
+namespace IMS.Application.WarehouseManagement.Services
+{
+    public interface IWarehouseTransactionDetailService
+    {
+        Task<List<WarehouseTransactionDetailDto>> GetAllTransactionsAsync(
+           string? projectName = null,
+           string? transactionType = null,
+           CancellationToken cancellationToken = default);
+    }
+}
---
+++ IMS/Areas/WarehouseManagement/Controllers/WarehouseTransactionDetailController.cs
@@ -0,0 +1,58 @@
+﻿using IMS.Application.ProjectManagement.Service;
+using IMS.Application.WarehouseManagement.DTOs;
+using IMS.Application.WarehouseManagement.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IMS.Areas.WarehouseManagement.Controllers
+{
+    [Area("WarehouseManagement")]
+    public class WarehouseTransactionDetailController : Controller
+    {
+        private readonly IWarehouseTransactionDetailService _transactionDetailService;
+        private readonly IApplicationDbContext _projectContext;
+        public WarehouseTransactionDetailController(
+        IWarehouseTransactionDetailService transactionDetailService,
+        IApplicationDbContext projectContext)  
+        {
+            _transactionDetailService = transactionDetailService;
+            _projectContext = projectContext;  
+        }
+
+        // GET: /WarehouseManagement/WarehouseTransactionDetail
+        // در کنترلر
+        public async Task<IActionResult> Index(string? projectName, string? transactionType)
+        {
+            var transactions = await _transactionDetailService.GetAllTransactionsAsync(projectName, transactionType);
+
+            // لیست پروژه‌ها
+            var projects = await _projectContext.Projects
+                .OrderBy(p => p.ProjectName)
+                .ToListAsync();
+
+            // نوع تراکنش‌ها بدون تکرار در مقدار فارسی
+            var transactionTypes = new Dictionary<string, string>
+    {
+        { "Receipt", "رسید" },
+        { "Issue", "حواله" },
+        { "Transfer", "انتقال" },
+        { "Consumption", "تبدیل" },
+        { "Production", "تبدیل" }
+    };
+
+            var uniqueTransactionTypes = transactionTypes
+                .GroupBy(kv => kv.Value)
+                .Select(g => g.First())
+                .ToDictionary(kv => kv.Key, kv => kv.Value);
+
+            ViewData["Projects"] = projects;
+            ViewData["TransactionType"] = transactionType;
+            ViewData["TransactionTypes"] = uniqueTransactionTypes;
+
+            return View(transactions);
+        }
+
+
+
+    }
+}
